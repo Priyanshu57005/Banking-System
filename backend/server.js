90d41cb8624be467253172be2c039cde99e1dfe7
Priyanshu Gautam <your-priyanshu57005@gmail.com>
2026-06-11 02:15:16 +0530

    Initial banking backend setup with user registration API

---
+++ backend/server.js
@@ -0,0 +1,15 @@
+require("dotenv").config() 
+
+const app = require('./src/app');
+const connectToDB = require("./src/config/db");
+
+connectToDB();
+
+
+
+
+ 
+const PORT = process.env.PORT || 3000;
+app.listen(PORT || 3000, () => {
+  console.log(`Server is running on port ${PORT}`);
+}); 
---
+++ backend/src/models/user.model.js
@@ -0,0 +1,46 @@
+const mongoose = require("mongoose")
+const bcrypt = require("bcryptjs")
+
+const userSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        required: [true, "Email is required to creating a user"],
+        trim: true,
+        lowercase: true,
+        match: [/[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$ /, "Invalid Email"],
+        unique:[true,"Email already exist"]
+    },
+    name: {
+        type: String,
+        required: [true, "Name is required to creating a website"]
+    },
+    password: {
+        type: String,
+        required: [true, "Password is required to creating an accound"],
+        minlength:[6,"password should contain more that 6 character"],
+        select: false
+    }
+},{
+    timestamps: true
+})
+
+userSchema.pre("save", async function (next) {
+    
+    if(!this.isModified("password")){
+        return next()
+    }
+
+    const hash = await bcrypt.hash(this.password, 10)
+    this.password = hash
+    return next()
+
+})
+
+userSchema.methods.comparePassword = async function (password) {
+
+    return await bcrypt.compare(password, this.password)
+}
+
+const userModel = mongoose.model("user", userSchema)
+
+module.exports = userModel
